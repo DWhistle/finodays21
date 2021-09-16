a6f1788f802407aa79e388df02ced587bc675f13
Andrew <shibaev.rus1@gmail.com>
2021-09-16 21:35:23 +0300

    deleted another repo

--- private/model/model.py
+++ private/model/model.py
@@ -13,5 +13,4 @@ def predict(text):
     inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**inputs)
     predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
-    # predicted = torch.argmax(predicted, dim=1).numpy()
     return PredictResult(predicted[0], predicted[1], predicted[2])
